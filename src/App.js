78e2598354d286e288b43c5ca2159ab63208eeaf
Johan Swetzén <johan@greenbyte.com>
2023-03-11 16:42:50 +0100

    Add basic functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import * as Icon from 'react-bootstrap-icons';
+
+function SendIRCommand(command) {
+  return fetch("http://192.168.1.72:1338/playIR/"+command)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="m-2 justify-content-md-center">
+        <Col></Col>
+        <Col><Button onClick={() => SendIRCommand("ReceiverOnOff")}><Icon.Power /></Button></Col>
+        <Col></Col>
+      </Row>
+      <Row className="m-2 justify-content-md-center">
+        <Col><Button onClick={() => SendIRCommand("ReceiverVolUp")}><Icon.CaretUpFill/></Button></Col>
+        <Col><Button onClick={() => SendIRCommand("ReceiverInput2")}>TV</Button></Col>
+        <Col><Button onClick={() => SendIRCommand("ReceiverInput3")}><Icon.Xbox /></Button></Col>
+      </Row>
+      <Row className="m-2 justify-content-md-center">
+        <Col>Volym</Col>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+      <Row className="m-2 justify-content-md-center">
+        <Col><Button onClick={() => SendIRCommand("ReceiverVolDown")}><Icon.CaretDownFill/></Button></Col>
+        <Col><Button onClick={() => SendIRCommand("ProjectorOnOff")}><Icon.Power /><br></br><Icon.ProjectorFill /></Button></Col>
+        <Col><Button onClick={() => SendIRCommand("ReceiverInput1")}><Icon.Laptop/></Button></Col>
+      </Row>
+    </Container>
   );
 }
 
